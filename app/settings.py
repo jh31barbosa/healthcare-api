02803e029a25cf9cea0a7bf44864abeb0c0c3c5e
Jose Henrique <99149994+jh31barbosa@users.noreply.github.com>
2025-04-26 06:43:44 -0300

    Changes on settings.py  including  apps on installed_apps,restframe_work backend configs and postgresSQL database

--- app/settings.py
+++ app/settings.py
@@ -77,23 +77,21 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
-
         'rest_framework.filters.SearchFilter',
         'rest_framework.filters.OrderingFilter',
     ],
 }
-
-
-
-
-
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'healthcare_db'),
+        'USER': os.getenv('POSTGRES_USER', 'healthcare_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'healthcare_pass'),
+        'HOST': os.getenv('POSTGRESS_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
